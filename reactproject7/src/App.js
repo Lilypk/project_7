1b58ece29209976048fdb49ed065f1c4ed66ec0f
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-11-05 18:46:55 -0500

    added component

--- reactproject7/src/App.js
+++ reactproject7/src/App.js
@@ -5,17 +5,38 @@ import Country from './Country'
 class App extends React.Component {
   constructor() {
     super()
-    
+    this.state = {
+      
+    }
   }
   
   componentDidMount() {
     fetch('http://localhost:9002/countries/')
       .then(res=>res.json())
-      .then(res=>console.log(res))
-
+      .then(data=> {
+        
+        this.setState({countryList: data})
+      console.log(this.state.countryList)
+      }
+        
+        )
+    
+   
   }
   render() {
-    return null
+    return (
+      <div className='container'>
+        <div className='body'>
+          <header>Countries</header>
+          <main>
+            {!this.state.countryList ? '' :this.state.countryList.map(country=>{
+              return <Country country = {country}/>
+            })}
+            {/* <Country country = {this.state.countryList} /> */}
+          </main>
+        </div>
+      </div>
+    )
   }
 }
 export default App
--- reactproject7/src/Country.js
+++ reactproject7/src/Country.js
@@ -1,15 +1,32 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-import './Country.css'
-
+import { render } from "@testing-library/react";
+import React from "react";
+import "./Country.css";
+import Popover from "react-bootstrap/popover";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 class Country extends React.Component {
-    constructor() {
-        super()
-    }
-   render() {
-       return null
-   }
+  constructor() {
+    super();
+   
+  }
+
+  render() {
+      console.log(this.props)
+    const popover = (
+        <Popover id="popover-basic">
+          <Popover.Title as="h3">Popover right</Popover.Title>
+          <Popover.Content>
+            And here's some <strong>amazing</strong> content. It's very engaging.
+            right?
+          </Popover.Content>
+        </Popover>
+      );
+    return (
+      <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+        <Button variant="success">Click me to see</Button>
+      </OverlayTrigger>
+    );
+  }
 }
-
-export default Country 
-
+export default Country;
