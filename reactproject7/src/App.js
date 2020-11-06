a132478886e33426f3e3f5ab9d8dbaad3900a9f3
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-11-06 16:16:40 -0500

    data on the page

--- reactproject7/src/App.js
+++ reactproject7/src/App.js
@@ -1,42 +1,36 @@
-import React from 'react'
-import './App.css'
-import Country from './Country'
+import React from "react";
+import "./App.css";
+import Country from "./Country";
 
 class App extends React.Component {
   constructor() {
-    super()
-    this.state = {
-      
-    }
+    super();
+    this.state = {};
   }
-  
+
   componentDidMount() {
-    fetch('http://localhost:9002/countries/')
-      .then(res=>res.json())
-      .then(data=> {
-        
-        this.setState({countryList: data})
-      console.log(this.state.countryList)
-      }
-        
-        )
-    
-   
+    fetch("http://localhost:9002/countries/")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ countryList: data });
+        console.log(this.state.countryList);
+      });
   }
   render() {
     return (
-      <div className='container'>
-        <div className='body'>
+      <div className="container">
+        <div className="body">
           <header>Countries</header>
           <main>
-            {!this.state.countryList ? '' :this.state.countryList.map(country=>{
-              return <Country country = {country}/>
-            })}
-           
+            {!this.state.countryList
+              ? ""
+              : this.state.countryList.map((country) => {
+                  return <Country country={country} />;
+                })}
           </main>
         </div>
       </div>
-    )
+    );
   }
 }
-export default App
+export default App;
--- reactproject7/src/Country.js
+++ reactproject7/src/Country.js
@@ -4,27 +4,30 @@ import "./Country.css";
 import Popover from "react-bootstrap/popover";
 import Button from "react-bootstrap/Button";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import "bootstrap/dist/css/bootstrap.min.css";
 class Country extends React.Component {
   constructor() {
     super();
-   
+    
   }
 
   render() {
-      console.log(this.props)
+    console.log(this.props);
     const popover = (
-        <Popover id="popover-basic">
-          <Popover.Title as="h3">Popover right</Popover.Title>
-          <Popover.Content>
-            And here's some <strong>amazing</strong> content. It's very engaging.
-            right?
-          </Popover.Content>
-        </Popover>
-      );
+      <Popover id="popover-basic" className='popover'>
+        <Popover.Title as="h3">Popover right</Popover.Title>
+        <Popover.Content>
+          {this.props.country.name}
+          {this.props.country.capital}
+          {this.props.country.region}
+          {this.props.country.languages[0].name}
+          {this.props.country.languages[0].nativeName}
+        </Popover.Content>
+      </Popover>
+    );
     return (
       <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-        <Button variant="success">Click me to see</Button>
+        <Button variant="success" className='buttons'>Click</Button>
       </OverlayTrigger>
     );
   }
